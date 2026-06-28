dcf45dbf1b600082dfb1c4fe08758e00607aff47
Ajay1498 <42824416+Ajay1498@users.noreply.github.com>
2026-06-28 11:32:51 +0545

    Enhance passenger_wsgi with traceback catching

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,7 +2,14 @@ import os
 import sys
 
 sys.path.insert(0, os.path.dirname(__file__))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_erp.settings')
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+try:
+    import crm_erp.wsgi
+    application = crm_erp.wsgi.application
+except Exception as e:
+    import traceback
+    def application(environ, start_response):
+        status = '500 Internal Server Error'
+        headers = [('Content-type', 'text/plain')]
+        start_response(status, headers)
+        return [traceback.format_exc().encode('utf-8')]
